24db32cd82eca93eabb7e1f45eae800bef95e13d
yeon uk Choi <vincentyeon@gmail.com>
2020-03-02 19:14:53 +0900

    Rest for the two week

---
+++ src/day01/Content.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Content {
+	
+	// First Question
+	// What is the Java?
+	//  - Computer Programming Language
+	//  - and Object Oriented Programming (<> Procedure Programming (C Language, ...))
+	// History of the Java
+	//  - Important 
+	//  - 1.5 - Generic, metadata(Annotation)
+	//  - 1.7 - Lambda(Function)
+	// Java Language Characteristic
+	// 
+	
+}
